e608bbd644517613434dc4bc830db4446a887475
Daniel <prince3257@gmail.com>
2022-01-04 17:01:48 +0800

    Update Info.swift

--- FinalProject/Data/Video/Info.swift
+++ FinalProject/Data/Video/Info.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct Info {
     
-    static let APIKEY = "AIzaSyBcwACZYmAyCPqQwZqD4C3dHzZ41yyVAFU"  //key=API_KEY
+    //static let APIKEY = "AIzaSyBcwACZYmAyCPqQwZqD4C3dHzZ41yyVAFU"  //key=API_KEY
     
-    static let OauthKEY = "806884348638-t8qh0groa8hir600eb60g408hcbhru6g.apps.googleusercontent.com"
+    //static let OauthKEY = "806884348638-t8qh0groa8hir600eb60g408hcbhru6g.apps.googleusercontent.com"
     
     static let YoutTubeFormat = "https://www.googleapis.com/youtube/v3/"
     
-    static let playlistID = "UUMUnInmOkrWN4gof9KlhNmQ"
+    //static let playlistID = "UUMUnInmOkrWN4gof9KlhNmQ"
     
     static var channelID = ""
     
